0eeac5c2ff4527ef7591539268ebb10568208b62
KaleabDereje <kaleabdereje388@gmail.com>
2024-09-02 18:54:48 +0300

    CORS cross origin resource share

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import {Port, mongoDB} from "./config.js";
 import router from "./routes/book.route.js";
@@ -12,6 +13,18 @@ app.use(express.json());
 //allow to use form format in the body and send to the API using POST
 app.use(express.urlencoded({ extended: false }));
 
+//Middleware for handling CORS Policy (cross origin resource sharing)
+//its a policy which prevent a web page to send requests to many domains at a time
+// use option 1: Allow origins with default of cors(*)
+            //app.use(cors());
+       // Option 2: Allow custom origins
+  app.use(
+    cors({
+        origin: 'http://localhost:8080',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+  );
 
 app.get('/', (req, res) =>{
     res.send("book store trial");
